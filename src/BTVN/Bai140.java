fed4478050b44efbd95e875382eef2c82d7b6d41
teoliver99 <tanhypym2@gmail.com>
2023-03-05 20:18:02 +0700

    Bai140

---
+++ src/BTVN/Bai140.java
@@ -0,0 +1,47 @@
+package BTVN;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Bai140 {
+    public static void main(String[] args) {
+        private double minPositiveNumber(double arr[],int n)
+        {
+            double min = -1;
+            int tempCount = 0;
+            for(int i = 0 ; i < n; i++){
+                if(arr[i] > 0){
+                    if(tempCount == 0){
+                        min = arr[i];
+                        tempCount ++;
+                    }else if(min > arr[i]){
+                        min = arr[i];
+                    }
+                }
+
+            }
+            return min;
+        }
+        public static void main(String[] args) {
+            int n;
+            System.out.print("Nhap n: ");
+            Scanner in = new Scanner(System.in);
+            n = in.nextInt();
+            in.close();
+
+            Bai140 minNumber = new Bai140();
+
+            if(n < 1){
+                System.out.print("Nhap lai");
+            }else{
+                double[] arr = new double[n];
+                Random random = new Random();
+                for (int i =0; i< n; i++){
+                    arr[i] = random.nextDouble(200)-100;
+                    System.out.printf("%.1f ",arr[i]);
+                }
+
+                double number = minNumber.minPositiveNumber(arr, n);
+                System.out.printf("\nGia tri duong nho nhat la: %.1f",number);
+    }
+}
